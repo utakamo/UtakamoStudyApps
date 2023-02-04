6334597f36f7734e27245bcbf8dc322e4f82d6b6
utakamo <utakamo.it@gmail.com>
2023-02-04 11:22:08 +0900

    Add libuci(c library) sample program for using uci_del_list func

---
+++ libuci-c/uci-sample08/uci-sample08.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <uci.h>
+#include <stdlib.h>
+
+//Source code description Web site URL
+//https://utakamo.com/article/openwrt/library/libuci-c.html
+
+int main (int argc, char **argv)
+{
+	struct uci_context *ctx;
+	struct uci_ptr ptr;
+	int ret = UCI_OK;
+
+	ctx = uci_alloc_context ();
+	
+	if (argc != 2) {
+		printf ("[uci_del_list]\n");
+		printf ("input argument error! specify uci parameter.\n");
+		printf ("ex) uci-sample08 <config>.<section>.<option>=<value>\n");
+		return 1;
+	}
+
+	uci_lookup_ptr (ctx, &ptr, argv[1], true);
+	
+	if (ptr.value)
+		ret = uci_del_list(ctx, &ptr);
+	else {
+		printf("Parser error\n");
+		ret = UCI_ERR_PARSE;	
+	}
+	
+	if (ret == UCI_OK)
+		uci_save(ctx, ptr.p);
+	
+	uci_free_context (ctx);
+
+	return 0;
+}
