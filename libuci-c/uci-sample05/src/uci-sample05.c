ee2b2f37729d8357d902f894892a32de8fcf737e
utakamo <utakamo.it@gmail.com>
2023-04-23 21:00:30 +0900

    Remove unnecessary space

--- libuci-c/uci-sample05/src/uci-sample05.c
+++ libuci-c/uci-sample05/src/uci-sample05.c
@@ -9,22 +9,22 @@
 void show_option_value(struct uci_ptr ptr);
 void show_list_value(struct uci_option *);
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	struct uci_context *ctx;
 	struct uci_ptr ptr;
 	int ret = UCI_OK;
 
-	ctx = uci_alloc_context ();
+	ctx = uci_alloc_context();
 	
 	if (argc != 2) {
-		printf ("input argument error! specify uci parameter.\n");
-		printf ("ex1) uci-sample05 <config>.<section>=<section-type>\n");
-		printf ("ex2) uci-sample05 <config>.<section>.<option>=<value>\n");
+		printf("input argument error! specify uci parameter.\n");
+		printf("ex1) uci-sample05 <config>.<section>=<section-type>\n");
+		printf("ex2) uci-sample05 <config>.<section>.<option>=<value>\n");
 		return 1;
 	}
 
-	uci_lookup_ptr (ctx, &ptr, argv[1], true);
+	uci_lookup_ptr(ctx, &ptr, argv[1], true);
 	
 	if (ptr.value)
 		ret = uci_set(ctx, &ptr);
@@ -36,7 +36,7 @@ int main (int argc, char **argv)
 	if (ret == UCI_OK)
 		uci_save(ctx, ptr.p);
 	
-	uci_free_context (ctx);
+	uci_free_context(ctx);
 
 	return 0;
 }
