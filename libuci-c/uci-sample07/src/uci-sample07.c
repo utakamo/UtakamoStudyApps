eba3ac37cffb843cdf5c9f528e960454d6520a4e
utakamo <utakamo.it@gmail.com>
2023-04-23 21:06:47 +0900

    Remove unnecessary space

--- libuci-c/uci-sample07/src/uci-sample07.c
+++ libuci-c/uci-sample07/src/uci-sample07.c
@@ -6,21 +6,21 @@
 //Source code description Web site URL
 //https://utakamo.com/article/openwrt/library/libuci-c.html
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	struct uci_context *ctx;
 	struct uci_ptr ptr;
 	int ret = UCI_OK;
 
-	ctx = uci_alloc_context ();
+	ctx = uci_alloc_context();
 	
 	if (argc != 2) {
-		printf ("input argument error! specify uci parameter.\n");
-		printf ("ex) uci-sample07 <config>.<section>.<option>=<value>\n");
+		printf("input argument error! specify uci parameter.\n");
+		printf("ex) uci-sample07 <config>.<section>.<option>=<value>\n");
 		return 1;
 	}
 
-	uci_lookup_ptr (ctx, &ptr, argv[1], true);
+	uci_lookup_ptr(ctx, &ptr, argv[1], true);
 	
 	if (ptr.value)
 		ret = uci_add_list(ctx, &ptr);
@@ -32,7 +32,7 @@ int main (int argc, char **argv)
 	if (ret == UCI_OK)
 		uci_save(ctx, ptr.p);
 	
-	uci_free_context (ctx);
+	uci_free_context(ctx);
 
 	return 0;
 }
