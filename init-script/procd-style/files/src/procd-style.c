19582356dd3e06e5ce7cd21d4ff44a5f98e16c66
utakamo <utakamo.it@gmail.com>
2023-05-15 21:09:19 +0900

    Add ubus support (incomplete)

--- init-script/procd-style/files/src/procd-style.c
+++ init-script/procd-style/files/src/procd-style.c
@@ -10,6 +10,9 @@
 #include <time.h>
 #include <ctype.h>
 #include <uci.h>
+#include <libubus.h>
+#include <json-c/json.h>
+#include <libubox/blobmsg_json.h>
 
 /* GLOBAL VARIABLE DECLARATION */
 bool terminate_flg = false;
@@ -19,6 +22,46 @@ void reply();
 bool uci_get_option(char*, char*);
 bool uci_set_option(char*);
 bool uci_commit_one_package(char*);
+bool procd_style_ubus_init();
+bool procd_style_reload();
+
+static int procd_style_handle_reload(struct ubus_context *ctx, struct ubus_object *obj,
+		     struct ubus_request_data *req, const char *method,
+		     struct blob_attr *msg)
+{
+	if (!procd_style_reload())
+		return UBUS_STATUS_NOT_FOUND;
+
+	return UBUS_STATUS_OK;
+}
+
+static struct ubus_method main_object_methods[] = {
+    { .name = "reload", .handler = procd_style_handle_reload },
+};
+
+static struct ubus_object_type main_object_type = UBUS_OBJECT_TYPE("procd-style", main_object_methods);
+
+static struct ubus_object main_object = {
+	.name = "procd-style",
+	.type = &main_object_type,
+	.methods = main_object_methods,
+	.n_methods = ARRAY_SIZE(main_object_methods),
+};
+
+static void procd_style_setup_signals(void)
+{
+	struct sigaction s;
+
+	memset(&s, 0, sizeof(s));
+	s.sa_handler = procd_style_handle_reload;
+	s.sa_flags = 0;
+	sigaction(SIGINT, &s, NULL);
+	sigaction(SIGTERM, &s, NULL);
+	sigaction(SIGUSR1, &s, NULL);
+
+	s.sa_handler = SIG_IGN;
+	sigaction(SIGPIPE, &s, NULL);
+}
 
 static void create_daemon()
 {
@@ -75,19 +118,9 @@ int main(int argc, char** argv)
 	signal(SIGTERM, sigterm_handler);
 	signal(SIGUSR1, sigusr1_handler);
 
-	reply();
+	procd_style_ubus_init();
 
-	for (;;) {
-		if (terminate_flg)
- 			break;
-
- 		if (reload_flg) {
- 			reply();
- 			reload_flg = false;
- 		}
-
- 		sleep(3);
-	}
+	uloop_run();
 
     return EXIT_SUCCESS;
 }
@@ -95,8 +128,8 @@ int main(int argc, char** argv)
 //Reload the uci configuration file.
 //Execution is triggered at startup and [/etc/init.d/procd-style reload].
 void reply() {
-	char user_name[256] = {0};
 	char greeting[256] = {0};
+	char user_name[128] = {0};
 
 	bool is_option = false;
 
@@ -162,7 +195,7 @@ bool uci_set_option(char* str) {
 	else {
 		ret = UCI_ERR_PARSE;
 		uci_free_context(ctx);
-		return -1;
+		return false;
 	}
 
 	if (ret == UCI_OK) {
@@ -197,3 +230,52 @@ bool uci_commit_one_package(char* str) {
 
 	return true;
 }
+
+bool procd_style_ubus_init() {
+	struct ubus_context *ctx;
+	struct ubus_object obj;
+
+	ctx = ubus_connect(NULL);
+
+	if (!ctx) {
+		fprintf(stderr, "Failed to connect to ubus\n");
+		return false;
+	}
+
+	//create ubus object
+	memset(&obj, 0, sizeof(obj));
+	obj.name = "procd-style";
+	obj.methods = (struct ubus_method[]) {
+		{.name = "reload", .handler = procd_style_handle_reload},
+		{}
+	};
+
+	//register ubus object
+	ubus_add_object(ctx, &obj);
+
+	ubus_free(ctx);
+
+	return true;
+}
+
+bool procd_style_reload() {
+
+	char user_name[128] = {0};
+	char greeting[256] = {0};
+
+	bool is_option = false;
+	//uci get procd-style.test.user
+	is_option = uci_get_option("procd-style.test.user", user_name);
+	
+	if (is_option) {
+		// uci set sysv-style.test.reply=Hello
+		snprintf(greeting, sizeof(greeting), "procd-style.test.reply=Hello %s!!", user_name);
+		uci_set_option(greeting);
+		// uci commit sysv-style
+		uci_commit_one_package("procd-style");
+	}	
+	
+	return is_option;
+}
+
+
