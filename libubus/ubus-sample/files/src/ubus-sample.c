ddad6b92188575316101d96b8f0e9d4c8077d6ec
utakamo <utakamo.it@gmail.com>
2023-07-30 21:10:59 +0900

    Fix ubus-sample package

--- libubus/ubus-sample/files/src/ubus-sample.c
+++ libubus/ubus-sample/files/src/ubus-sample.c
@@ -21,38 +21,63 @@
 #define HIT_UCI_OPTION			4
 #define UCI_SEARCH_NOT_FOUND		5
 
-static struct blob_buf blob;
-
-/* Ubus method policy */
-static const struct blobmsg_policy change_option_policy[] =
-{
-	{ .name="option", .type=BLOBMSG_TYPE_STRING },
-	{ .name="value", .type=BLOBMSG_TYPE_STRING },
+enum {
+	UCI_SET_INFO_OPTION,
+	UCI_SET_INFO_VALUE,
+	UCI_SET_INFO_MAX,
 };
 
-static const struct blobmsg_policy output_option_policy[] = {};
+static struct blob_buf blob;
+
+/* */
+struct uci_set_data {
+	struct avl_node avl;
+	char *option;
+	char *value;
+};
+
+struct avl_tree uci_set_datas;
+
+const char* find_uci_option(const char *user_input)
+{
+	struct uci_set_data *o;
+	o = avl_find_element(&uci_set_datas, user_input, o, avl);
+	if (!o)
+		return NULL;
+	return o;
+}
+
+/* Ubus method policy */
+static const struct blobmsg_policy check_json_format_policy[] = {};
+static const struct blobmsg_policy show_uci_option_policy[] = {};
+static const struct blobmsg_policy update_uci_option_policy[] =
+{
+	[UCI_SET_INFO_OPTION] = { .name="option", .type=BLOBMSG_TYPE_STRING },
+	[UCI_SET_INFO_VALUE] = { .name="value", .type=BLOBMSG_TYPE_STRING },
+};
+
 
 /* ubus methods */
-static int change_option(struct ubus_context *ctx, struct ubus_object *obj,
+static int check_json_format(struct ubus_context *ctx, struct ubus_object *obj,
 			  struct ubus_request_data *req, const char *method,
 			  struct blob_attr *msg);
 
-static int output_option(struct ubus_context *ctx, struct ubus_object *obj,
+static int show_uci_option(struct ubus_context *ctx, struct ubus_object *obj,
 			  struct ubus_request_data *req, const char *method,
 			  struct blob_attr *msg);
+
+static int update_uci_option(struct ubus_context *ctx, struct ubus_object *obj,
+			  struct ubus_request_data *req, const char *method,
+			  struct blob_attr *msg);
+
 
 void ubus_process(void);
-
-int uci_search_option(const char* str);
 
 //Function equivalent to the uci get command.
 bool uci_get_option(char* str, char* value);
 
 //Function equivalent to the uci set command.
 bool uci_set_option(char* str);
-
-//Function equivalent to the uci commit command.
-bool uci_commit_one_package(char* str);
 
 /* GLOBAL VARIABLE DECLARATION */
 bool terminate_flg = false;
@@ -124,28 +149,27 @@ int main(int argc, char** argv)
 // Ubus method functions
 //output the uci configuration file.
 //Execution is triggered at startup and [/etc/init.d/ubus-sample].
-int change_option(struct ubus_context *ctx, struct ubus_object *obj,
+int check_json_format(struct ubus_context *ctx, struct ubus_object *obj,
 				struct ubus_request_data *req, const char *method,
 				struct blob_attr *msg) {
-
 	void *s;
 	blob_buf_init(&blob, 0);
-	blobmsg_add_string(&blob, "reply", "Hmm? Did you call me?");
-	blobmsg_add_string(&blob, "description", "Output sample data as follows");
-	blobmsg_add_string(&blob, "string", "Welcome to ubus world!");
-	blobmsg_add_u8(&blob, "bool1", true);
-	blobmsg_add_u8(&blob, "bool2", false);
+	blobmsg_add_string(&blob, "Description", "[ubus json format]");
+	blobmsg_add_string(&blob, "string", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	blobmsg_add_u8(&blob, "bool", true);
 	blobmsg_add_u32(&blob, "numeric data", 100);
 	s = blobmsg_open_table(&blob, "table");
-	blobmsg_add_string(&blob, "element1", "string data");
-	blobmsg_add_u8(&blob, "element2", true);
-	blobmsg_add_u32(&blob, "element3", 200);
+	blobmsg_add_string(&blob, "table-element1", "string data");
+	blobmsg_add_u8(&blob, "table-element2", true);
+	blobmsg_add_u32(&blob, "table-element3", 200);
 	blobmsg_close_table(&blob, s);
+	blobmsg_add_string(&blob, "Note", "Please read this source code for detailed usage.");
 	ubus_send_reply(ctx, req, blob.head);
+
 	return 0;
 }
 
-int output_option(struct ubus_context *ctx, struct ubus_object *obj,
+int show_uci_option(struct ubus_context *ctx, struct ubus_object *obj,
 			  struct ubus_request_data *req, const char *method,
 			  struct blob_attr *msg) {
 
@@ -160,9 +184,59 @@ int output_option(struct ubus_context *ctx, struct ubus_object *obj,
 	uci_get_option("ubus-sample.test.data3", data3);
 
 	blob_buf_init(&blob, 0);
+	blobmsg_add_string(&blob, "Description", "Output /etc/config/ubus-sample");
 	blobmsg_add_string(&blob, "data1", data1);
 	blobmsg_add_string(&blob, "data2", data2);
 	blobmsg_add_string(&blob, "data3", data3);
+	ubus_send_reply(ctx, req, blob.head);
+
+	return 0;
+}
+
+
+int update_uci_option(struct ubus_context *ctx, struct ubus_object *obj,
+			  struct ubus_request_data *req, const char *method,
+			  struct blob_attr *msg) {
+
+	struct blob_attr *tb[UCI_SET_INFO_MAX];
+
+	blobmsg_parse(update_uci_option_policy, UCI_SET_INFO_MAX, tb, blob_data(msg), blob_len(msg));
+
+	if ((!tb[UCI_SET_INFO_OPTION]) || (!tb[UCI_SET_INFO_VALUE])){
+		blob_buf_init(&blob, 0);
+		blobmsg_add_string(&blob, "Result", "[UCI SET FAILED] NO INPUT JSON DATA!!");
+		ubus_send_reply(ctx, req, blob.head);
+
+		return -1;
+	}
+
+	const char *option = blobmsg_get_string(tb[UCI_SET_INFO_OPTION]);
+	const char *value = blobmsg_get_string(tb[UCI_SET_INFO_VALUE]);
+
+	if (!option || !value) {
+		blob_buf_init(&blob, 0);
+		blobmsg_add_string(&blob, "Result", "[UCI SET FAILED] NOT FOUND UCI OPTION!!");
+		ubus_send_reply(ctx, req, blob.head);
+
+		return -1;
+	}
+
+	char update_data[1024];
+	snprintf(update_data, sizeof(update_data), "ubus-sample.test.%s=%s", option, value);
+
+	bool result = uci_set_option(update_data);
+
+	if (!result) {
+		blob_buf_init(&blob, 0);
+		blobmsg_add_string(&blob, "Result", "[UCI SET FAILED] UCI ERROR!!");
+		ubus_send_reply(ctx, req, blob.head);
+
+		return -1;
+	}
+
+	blob_buf_init(&blob, 0);
+	blobmsg_add_string(&blob, "Result", "[UCI SET SUCCESS] Please check ubus-sample config!!");
+	blobmsg_add_string(&blob, "Note", "ex) user~# uci show ubus-sample");
 	ubus_send_reply(ctx, req, blob.head);
 
 	return 0;
@@ -172,8 +246,9 @@ int output_option(struct ubus_context *ctx, struct ubus_object *obj,
 const struct ubus_method ubus_sample_methods[] =
 {
 	/* UBUS_METHOD(method_name, method_call_function, method_policy) */
-	UBUS_METHOD("change_option", change_option, change_option_policy),
-	UBUS_METHOD("output_option", output_option, output_option_policy),
+	UBUS_METHOD("check_json_format", check_json_format, check_json_format_policy),
+	UBUS_METHOD("show_uci_option", show_uci_option, show_uci_option_policy),
+	UBUS_METHOD("update_uci_option", update_uci_option, update_uci_option_policy),
 };
 
 /* Ubus object type */
@@ -197,52 +272,6 @@ void ubus_process(void) {
 	uloop_run();
 	uloop_done();
 	return;
-}
-
-int uci_search_option(const char* str) {
-	struct uci_context *ctx;
-	struct uci_ptr ptr;
-	struct uci_element *e;
-	char* input_option = NULL;
-
-	char* param = strdup(str);
-
-	ctx = uci_alloc_context();
-
-	if (param == NULL) {
-		return FAILED_STRDUP;
-	}
-
-	if (ctx == NULL) {
-		return FAILED_ALLOC_UCI_CONTEXT;
-	}
-
-	strtok(param, ".");			//extract <config>
-	strtok(NULL, ".");			//extract <section>
-	input_option = strtok(NULL, ".");	//extract <option>
-
-	if (input_option == NULL) {
-		return FAILED_EXTRACT_UCI_PARAMETER;
-	}
-
-	int lookup_status = uci_lookup_ptr(ctx, &ptr, param, true);
-
-	if (lookup_status != UCI_OK) {
-		return lookup_status;
-	}
-
-	if (ptr.o != NULL) {
-		uci_foreach_element(&ptr.s->options, e) {
-			struct uci_option *o = uci_to_option(e);
-			if (o->type == UCI_TYPE_STRING || o->type == UCI_TYPE_LIST) {
-				if (!strcmp(o->e.name, input_option)) {
-					return HIT_UCI_OPTION;
-				}
-			}
-		}
-	}
-
-	return UCI_SEARCH_NOT_FOUND;
 }
 
 //Function equivalent to the uci get command.
@@ -309,29 +338,5 @@ bool uci_set_option(char* str) {
 	}
 
 	uci_free_context(ctx);
-	return true;
-}
-
-//Function equivalent to the uci commit command.
-bool uci_commit_one_package(char* str) {
-	struct uci_context *ctx;
-	struct uci_ptr ptr;
-
-	char* config = strdup(str);
-
-	ctx = uci_alloc_context();
-
-	if (uci_lookup_ptr(ctx, &ptr, config, true) != UCI_OK) {
-		uci_perror(ctx, "uci commit error");
-		return false;
-	}
-
-	if (ptr.p != 0) {
-		if (uci_commit(ctx, &ptr.p, true) != UCI_OK) {
-			uci_perror(ctx, "uci commit error");
-			return false;
-		}
-	}
-
 	return true;
 }
