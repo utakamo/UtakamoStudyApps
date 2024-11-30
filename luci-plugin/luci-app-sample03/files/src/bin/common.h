bcc345d0bd3a35842a04c71d0d279a12df286e04
utakamo <utakamo.it@gmail.com>
2024-11-30 12:13:43 +0900

    Add common.h

---
+++ luci-plugin/luci-app-sample03/files/src/bin/common.h
@@ -0,0 +1,14 @@
+#define ERR_SOCKET          1
+#define ERR_INET_PTON       2
+#define ERR_INET_PTON_DST   3
+#define ERR_INET_PTON_GT    4
+#define ERR_INET_PTON_MASK  5
+#define ERR_IOCTL           6
+#define ERR_MAC_FORMAT      7
+#define ERR_BIND            8
+#define ERR_SEND            9
+#define ERR_RECV            10
+#define ERR_RESPONSE        11
+
+#define MAX_INTERFACES      128
+#define MAX_IFNAME_LEN      256
